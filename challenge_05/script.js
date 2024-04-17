50de9b4788174d9f11777577714e823b0e331be7
youssef <benotsman.youssuf@gmail.com>
2024-04-17 09:43:08 +0100

    edit left GB number

--- challenge_05/script.js
+++ challenge_05/script.js
@@ -1,11 +1,12 @@
-const countElement = document.getElementById('number');
-let count = 0;
+const countElement = document.getElementById('number'); // Get the element with id 'number'
+let count = 0; // Initialize the count variable to 0
+
 async function startCounting() {
-  for (let i = 0; i <= 185; i++) {
-    countElement.textContent = count;
-    await new Promise((resolve) => setTimeout(resolve, 15));
-    count++;
+  for (let i = 0; i <= 185; i++) { // Loop from 0 to 185
+    countElement.textContent = count; // Update the text content of the countElement with the current count value
+    await new Promise((resolve) => setTimeout(resolve, 15)); // Wait for 15 milliseconds using a Promise and setTimeout
+    count++; // Increment the count variable by 1
   }
 }
-window.onload = startCounting;
 
+window.onload = startCounting; // Call the startCounting function when the window has finished loading
